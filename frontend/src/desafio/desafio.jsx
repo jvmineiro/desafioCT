45c64885c1a97295f98a60a5216eb52b13f1e3eb
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-12-21 10:17:31 -0300

    Mais componentes adicionados

---
+++ frontend/src/desafio/desafio.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Desafio extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Desafio</h1>
+            </div>
+        )
+    }
+}
